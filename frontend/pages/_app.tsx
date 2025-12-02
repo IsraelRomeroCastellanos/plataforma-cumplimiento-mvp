395950da8d25260aff35a97097f7b18b3b5b9f3f
Israel <israelrc@ntegix.com>
2025-12-02 14:18:31 -0600

    fix: agregar dependencia react-toastify para notificaciones

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,27 @@
+// frontend/pages/_app.tsx
+import '../styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+export default MyApp;
